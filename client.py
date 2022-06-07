f167baa340f9193fd11c0f67e4d11133c6bce4b9
ChuDucAnh242002 <anh.chu@tuni.fi>
2022-06-07 09:13:16 +0300

    create initial files

---
+++ client.py
@@ -0,0 +1,27 @@
+""" 
+    author: Chu Duc Anh
+    Github: https://github.com/ChuDucAnh242002
+    A game of calculate to 98 created High School Le Hong Phong in Viet Nam
+"""
+
+import pygame
+import sys
+import os
+ 
+pygame.init()
+
+def main():
+    run = True
+    # while run:
+
+
+
+def menu():
+    run = True
+    while run:
+
+
+
+if __name__ == "__main__":
+    while True:
+        menu()
